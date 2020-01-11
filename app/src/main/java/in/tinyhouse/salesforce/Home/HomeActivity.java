91a5bc3bce61e20920b84fba695f1fc3331534b0
MAYANK KUMAR <sonunayan48@gmail.com>
2020-01-11 14:14:36 +0530

    Completed slide up panel feature.

--- app/src/main/java/in/tinyhouse/salesforce/Home/HomeActivity.java
+++ app/src/main/java/in/tinyhouse/salesforce/Home/HomeActivity.java
@@ -1,16 +1,53 @@
 package in.tinyhouse.salesforce.Home;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
 
 import in.tinyhouse.salesforce.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private SlidingUpPanelLayout mLayout;
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mLayout = findViewById(R.id.slideup);
+        mText = findViewById(R.id.salesforce);
+
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                if (newState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+                    mText.setText("Today's activity");
+                } else if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+                    mText.setText("salesforce");
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mLayout != null && mLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED || mLayout.getPanelState() == SlidingUpPanelLayout.PanelState.ANCHORED) {
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+        } else {
+            super.onBackPressed();
+        }
     }
 }

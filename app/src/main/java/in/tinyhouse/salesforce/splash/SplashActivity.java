18a038eb9dc92b532ce1835ad746882d3cbf7ce2
parthsharma07 <parthcricketer07@gmail.com>
2020-10-03 18:14:10 +0530

    Update SplashActivity.java (#48)
    Splash time changed

--- app/src/main/java/in/tinyhouse/salesforce/splash/SplashActivity.java
+++ app/src/main/java/in/tinyhouse/salesforce/splash/SplashActivity.java
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                 finish();
             }
-        }, 4000);
+        }, 5000);
     }
 }
